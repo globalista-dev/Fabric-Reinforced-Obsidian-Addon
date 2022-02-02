b8bc332470b3b667a5d26c484b3afa2a740f49a6
Globalista <35036442+globalista-dev@users.noreply.github.com>
2022-02-02 18:48:35 -0300

    Added items

--- src/main/java/com/globalista/romod/item/ModItems.java
+++ src/main/java/com/globalista/romod/item/ModItems.java
@@ -12,4 +12,10 @@ public class ModItems implements ItemRegister {
 
     public static final Item DARK_IRON_INGOT = ItemRegister.registerItem("dark_iron_ingot",
             new Item(new FabricItemSettings().group(RoMod.ROGROUP)));
+
+    public static final Item NETHERITE_DUST = ItemRegister.registerItem("netherite_dust",
+            new Item(new FabricItemSettings().group(RoMod.ROGROUP)));
+
+    public static final Item UNCOOKED_DARK_IRON_DUST = ItemRegister.registerItem("uncooked_dark_iron_dust",
+            new Item(new FabricItemSettings().group(RoMod.ROGROUP)));
 }
